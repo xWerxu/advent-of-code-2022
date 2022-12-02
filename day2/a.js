733b9730d2ad4436e32540b7f7a60b7c014cc4c5
Piotr Skwierczynski <piotr.skwierczynski@rtbhouse.com>
2022-12-02 10:02:19 +0100

    solution A

---
+++ day2/a.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const { default: test } = require("node:test");
+
+function roundResult(input){
+    const scoreCard = {
+        X: 1, // rock
+        Y: 2, // paper
+        Z: 3, // scissor
+        win: 6,
+        draw: 3,
+        lost: 0
+    }
+
+    // A : rock
+    // B : paper
+    // C : scissors
+
+    // rock w: scissors l:paper
+    // paper w: rock l:scissors
+    // scissors w: paper l:rock
+
+
+    let inputSplited = input.split(" ");
+    let val1 = inputSplited[0];
+    let val2 = inputSplited[1];
+    // console.log(inputSplited)
+    switch(val1){
+        case "A":
+            if(val2 == "X") return scoreCard.X + scoreCard.draw;
+            if(val2 == "Y") return scoreCard.Y + scoreCard.win;
+            if(val2 == "Z") return scoreCard.Z + scoreCard.lost;
+            break;
+        case "B":
+            if(val2 == "X") return scoreCard.X + scoreCard.lost;
+            if(val2 == "Y") return scoreCard.Y + scoreCard.draw;
+            if(val2 == "Z") return scoreCard.Z + scoreCard.win;
+            break;
+        case "C":
+            if(val2 == "X") return scoreCard.X + scoreCard.win;
+            if(val2 == "Y") return scoreCard.Y + scoreCard.lost;
+            if(val2 == "Z") return scoreCard.Z + scoreCard.draw;
+            break;
+                    
+    }
+
+}
+
+
+fs.readFile("./input.txt","utf-8",(err, data) => {
+    const dataSplited = data.split("\n");
+    let score = 0;
+    dataSplited.forEach((val)=>{
+        // console.log(roundResult(val))
+        score += roundResult(val)
+    })
+    console.log(score)
+})
+
+
